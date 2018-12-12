ff18573e50d3adf6072b766a97361bc94ebeca86
Masafumi Koba <ybiquitous@gmail.com>
2018-12-12 22:23:07 +0900

    Fix error when End-of-Line pattern
    ```
    TypeError: no implicit conversion of nil into String
        /Users/koba/git/ybiquitous/goodcheck/lib/goodcheck/reporters/text.rb:29:in `+'
        /Users/koba/git/ybiquitous/goodcheck/lib/goodcheck/reporters/text.rb:29:in `issue'
        /Users/koba/git/ybiquitous/goodcheck/lib/goodcheck/commands/check.rb:35:in `block (4 levels) in run'
        /Users/koba/git/ybiquitous/goodcheck/lib/goodcheck/analyzer.rb:40:in `each'
        /Users/koba/git/ybiquitous/goodcheck/lib/goodcheck/analyzer.rb:40:in `scan'
        /Users/koba/git/ybiquitous/goodcheck/lib/goodcheck/commands/check.rb:33:in `block (3 levels) in run'
        /Users/koba/git/ybiquitous/goodcheck/lib/goodcheck/reporters/text.rb:19:in `rule'
        /Users/koba/git/ybiquitous/goodcheck/lib/goodcheck/commands/check.rb:31:in `block (2 levels) in run'
        /Users/koba/git/ybiquitous/goodcheck/lib/goodcheck/commands/check.rb:79:in `block (6 levels) in each_check'
        /Users/koba/git/ybiquitous/goodcheck/lib/goodcheck/config.rb:22:in `each'
        /Users/koba/git/ybiquitous/goodcheck/lib/goodcheck/config.rb:22:in `rules_for_path'
        /Users/koba/git/ybiquitous/goodcheck/lib/goodcheck/commands/check.rb:66:in `block (5 levels) in each_check'
        /Users/koba/git/ybiquitous/goodcheck/lib/goodcheck/reporters/text.rb:15:in `file'
        /Users/koba/git/ybiquitous/goodcheck/lib/goodcheck/commands/check.rb:63:in `block (4 levels) in each_check'
        ...
    ```

--- lib/goodcheck/reporters/text.rb
+++ lib/goodcheck/reporters/text.rb
@@ -20,14 +20,14 @@ module Goodcheck
       end
 
       def issue(issue)
-        line = issue.buffer.line(issue.location.start_line).chomp
+        line = issue.buffer.line(issue.location.start_line)
         end_column = if issue.location.start_line == issue.location.end_line
                        issue.location.end_column
                      else
                        line.bytesize
                      end
-        colored_line = line.byteslice(0, issue.location.start_column) + Rainbow(line.byteslice(issue.location.start_column, end_column - issue.location.start_column)).red + line.byteslice(end_column, line.bytesize - end_column)
-        stdout.puts "#{issue.path}:#{issue.location.start_line}:#{colored_line}:\t#{issue.rule.message.lines.first.chomp}"
+        colored_line = line.byteslice(0, issue.location.start_column) + Rainbow(line.byteslice(issue.location.start_column, end_column - issue.location.start_column)).red + line.byteslice(end_column, line.bytesize)
+        stdout.puts "#{issue.path}:#{issue.location.start_line}:#{colored_line.chomp}:\t#{issue.rule.message.lines.first.chomp}"
       end
     end
   end
